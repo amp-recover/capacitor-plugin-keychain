8b35ce1cf9a713e0690746433f095f473ff2956f
John Munro <jbmunro4@gmail.com>
2021-02-02 15:15:06 -0500

    ios plugin does authenticate

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,4 +5,9 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(Keychain, "Keychain",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isAvailable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(authenticate, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(registerBiometricSecret, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(loadBiometricSecret, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(removeBiometricSecret, CAPPluginReturnPromise);
 )
--- src/web.ts
+++ src/web.ts
@@ -8,9 +8,25 @@ export class KeychainWeb extends WebPlugin implements KeychainPlugin {
       platforms: ['web'],
     });
   }
+  isAvailable(options: { allowBackup: boolean; }): Promise<{ biometryType: string; code: string; message: string; }> {
+    console.log('ECHO?', options);
+    // return options;
+    throw new Error('Method not implemented.');
+  }
+  registerBiometricSecret(options: { secret: string; invalidateOnEnrollment: boolean; }): Promise<{ message: string; code: string; }> {
+    console.log('ECHO?', options);
+    throw new Error('Method not implemented.');
+  }
+  loadBiometricSecret(options: { description: string; }): Promise<{ secret: string, message: string; code: string; }> {
+    console.log('ECHO?', options);
+    throw new Error('Method not implemented.');
+  }
+  removeBiometricSecret(): Promise<{ message: string; code: string; }> {
+    throw new Error('Method not implemented.');
+  }
 
   async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
+    console.log('ECHO?', options);
     return options;
   }
 }
